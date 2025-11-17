b4c10b8fb69eda399e4f7968964858a494042271
Festus <temiidowu321@gmail.com>
2025-11-17 10:13:01 +0100

    Commit

--- server.js
+++ server.js
@@ -10,6 +10,14 @@ server.use(jsonServer.bodyParser);
 
 server.use('/api', router);
 
+server.use((req, res, next) => {
+  res.header('Access-Control-Allow-Origin', '*'); // Allows all origins, you can restrict to localhost:4200 if needed
+  res.header('Access-Control-Allow-Methods', 'GET,POST,PATCH,DELETE,OPTIONS');
+  res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
+  next();
+});
+
+
 const PORT = process.env.PORT || 3000;
 server.listen(PORT, () => {
   console.log('JSON Server running on port ' + PORT);
